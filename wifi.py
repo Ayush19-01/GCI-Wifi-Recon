c28d95755f28f9f0e5e292fca878d04dbc471a66
Ayush19-01 <57313104+Ayush19-01@users.noreply.github.com>
2020-01-14 16:54:39 +0530

    Update wifi.py

--- wifi.py
+++ wifi.py
@@ -1,7 +1,9 @@
+#Made for the sole purpose of GCI 2019
+import re
 import subprocess
 import threading
-from getmac import get_mac_address
 import os
+import time
 os.system("clear")
 
 def prRed(skk): return str("\033[91m{}\033[00m" .format(skk)) 
@@ -15,42 +17,30 @@ a=input(prRed("Press enter to scan your wifi network and get information related
 print(prGreen("\nScanning...\n"))
 list0 = []
 list1 = []
-list2 = []
-def recon(i):
-    try:
-        ip_address = '192.168.1.'+str(i)
-        state = 'ping -c 1 '+ip_address
-        result = subprocess.check_output(state.split()).decode().split('\n')
-        
-        for get in result:
-            if '0% packet loss' in get: 
-                list0.append(ip_address)
- 
-    except Exception as error:
-        pass
-
-for i in range(1, 255):
-    try:
-        t = threading.Thread(target=recon,args=(i, ))
-        t.start()
-    except Exception as error:
-        print(prRed('Error Occurred!'))
-        
-
-
-for k in list0:
-    mac = get_mac_address(ip=k)
-    if mac != None:
-    	list1.append(str(mac))
-    	list2.append(str(k))
+def fetch_data():
+	global list0
+	global list1
+	arp_out = str(subprocess.check_output(['arp','-a']))
+	x=arp_out.split()
+	list0=[]
+	list1=[]
+	for i in x:
+		if ":" in i:
+			list0.append(i)
+		if "." in i:
+			list1.append(i)
+		else:
+			continue
+fetch_data()
+time.sleep(3)
 print(prYellow("Number of devices connected to your wifi : "),prCyan(str(len(list1))))
 print()
-print(prRed("         {:26}{}".format("Mac Address","Ip-Address\n")))
+print(prRed("        {:30}{}".format("Ip-Address","Mac Address\n")))
 x=0
 count=1
 for l in list1:
-	print("{}      {:38}{}".format(prGreen(count),prCyan(l),prPurple(list2[x])))
+	print("{}      {:38}{}".format(prGreen(count),prCyan(l),prPurple(list0[x])))
 	print()
 	x += 1
 	count += 1
-print(prRed("Closing..."))
+print(prRed("Closing...\n"))
